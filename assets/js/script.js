0ed2eb3cec284d10795d5405cefe00ddf9dc85d7
umair shah <umairshah14@hotmail.com>
2023-01-28 23:08:01 +0000

    more updates, first api call to get long and lat units

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
-var searchCity = document.querySelector("#search-input");
 var searchBtnEl = $("#search-button");
 var currentDayEl = $("#currentDay");
 
-var today = moment()
-$(currentDayEl).text(searchCity.value + today.format("dddd, MMMM Do"));
+var searchCity = "";
 
 searchBtnEl.on("click", function (event) {
   event.preventDefault();
-  searchCity = $(searchCity).val()
+  searchCity = document.querySelector("#search-input").value;
   console.log("city is: " + searchCity);
-});
 
-var citytoLonLatURL = "http://api.openweathermap.org/geo/1.0/direct?q=" +  searchCity.val + "&limit=1&appid=11a6edf7f55109a8876a082e0f89437e"
-$.ajax({
-    url : citytoLonLatURL,
-    method : "GET"
-}).then (function (response) {
-    console.log(response);
-})
+  //SETS THE DATE
+  var today = moment();
+  $(currentDayEl).text(searchCity + today.format(" (dddd, MMMM Do)"));
+
+  var citytoLonLatURL = "http://api.openweathermap.org/geo/1.0/direct?q=" +  searchCity + "&limit=1&appid=11a6edf7f55109a8876a082e0f89437e"
+  $.ajax({
+      url : citytoLonLatURL,
+      method : "GET"
+  }).then (function (response) {
+      console.log(response);
+  })
+});
