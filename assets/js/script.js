84e48b55c7ef71856f7d687536a102fd820e4c74
umair shah <umairshah14@hotmail.com>
2023-01-28 22:52:30 +0000

    more updates, started work on scipt.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
-var searchCity = $("#search-input").val();
-var searchBtnEl = $("#search-button")
+var searchCity = document.querySelector("#search-input");
+var searchBtnEl = $("#search-button");
+var currentDayEl = $("#currentDay");
 
-console.log(searchCity);
+var today = moment()
+$(currentDayEl).text(searchCity.value + today.format("dddd, MMMM Do"));
 
-searchBtnEl.on("click", function(event){
-    event.preventDefault();
-    console.log("city is: " + searchCity);
+searchBtnEl.on("click", function (event) {
+  event.preventDefault();
+  searchCity = $(searchCity).val()
+  console.log("city is: " + searchCity);
+});
+
+var citytoLonLatURL = "http://api.openweathermap.org/geo/1.0/direct?q=" +  searchCity.val + "&limit=1&appid=11a6edf7f55109a8876a082e0f89437e"
+$.ajax({
+    url : citytoLonLatURL,
+    method : "GET"
+}).then (function (response) {
+    console.log(response);
 })
