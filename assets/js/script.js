12ba0512b683551e1c0b0eb7c65689a6788e3724
umair shah <umairshah14@hotmail.com>
2023-01-28 23:41:51 +0000

    more updates, working on second ajax call

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var searchBtnEl = $("#search-button");
 var currentDayEl = $("#currentDay");
 var searchCity = "";
 var today = moment();
+var forecastURL = ""
 $(currentDayEl).text("London" + today.format(" (MM/DD/YYYY)"));
 
 searchBtnEl.on("click", function (event) {
@@ -14,27 +15,29 @@ searchBtnEl.on("click", function (event) {
   $(currentDayEl).text(searchCity + today.format(" (MM/DD/YYYY)"));
 
   // PASSES THE CITY INTO THE URL TO GRAB THE LONGITUDE AND LATITUDE
-  var LonLatURL = "http://api.openweathermap.org/geo/1.0/direct?q=" +  searchCity + "&limit=1&appid=11a6edf7f55109a8876a082e0f89437e"
+  var LonLatURL ="http://api.openweathermap.org/geo/1.0/direct?q=" + searchCity + "&limit=1&appid=11a6edf7f55109a8876a082e0f89437e";
 
   // FIRST AJAX API CALL GIVES THE LONGITUDE AND LATITUDE CO ORDINATES
   $.ajax({
-      url : LonLatURL,
-      method : "GET"
-  }).then (function (response) {
-    var lat = response[0].lon
-    var lon = response[0].lat
+    url: LonLatURL,
+    method: "GET",
+  }).then(function (response) {
+    var lat = response[0].lon;
+    var lon = response[0].lat;
     console.log("Longitude: " + lat);
     console.log("Latitude: " + lon);
-  })
+  });
+
 
   // SECOND AJAX CALL USES LONGITUDE AND LATITUDE DEFINED IN PREV AJAX CALL
-  var forecastURL = "api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=5&appid=11a6edf7f55109a8876a082e0f89437e"
-
+  forecastURL ="api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon +"&cnt=5&appid=11a6edf7f55109a8876a082e0f89437e";
+    
   $.ajax({
-    url: forecastURL,
-    method: "GET",
+      url: forecastURL,
+      method: "GET",
   }).then(function (responseForecast) {
+      console.log(responseForecast);
+  });
 
-    console.log(responseForecast);
-  })
+  //EVENT LISTENER CLOSES
 });
