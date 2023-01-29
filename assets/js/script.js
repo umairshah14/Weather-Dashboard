67f8d055bc00ae4000093d3fdf6846d885b63677
umair shah <umairshah14@hotmail.com>
2023-01-29 12:31:18 +0000

    more updates, grabbed the date for each forecast day

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ var forecastURL = ""
 var lat = 0
 var lon = 0
 var searchHistory = []
+
 var newButton = $('<button>')
 var newDiv = $("<div>")
 var newh5 = $("<h5>")
@@ -15,6 +16,7 @@ var newImg = $("<img>")
 var today = moment();
 $(currentDayEl).text("LONDON, GB" + today.format(" (MM/DD/YYYY)"));
 
+
 function renderSearchHistory(searchHistory) {
         var newButton = $("<button>");
         newButton.text(searchHistory);
@@ -22,32 +24,42 @@ function renderSearchHistory(searchHistory) {
         additionalCitiesDiv.append(newButton);
 }
 
+
 searchBtnEl.on("click", function (event) {
   event.preventDefault();
 
   // GRABS THE CITY USER TYPES IN
   searchCity = document.querySelector("#search-input").value.toUpperCase();
-
   console.log("city is: " + searchCity);
   searchHistory = searchCity
   console.log(searchHistory);
   renderSearchHistory(searchHistory)
 
+  // PASSES THE CITY INTO THE URL TO GRAB THE LONGITUDE AND LATITUDE
+  var LonLatURL ="http://api.openweathermap.org/geo/1.0/direct?q=" + searchCity + "&limit=1&appid=11a6edf7f55109a8876a082e0f89437e"; 
 
-  // CREATE URL TO RUN THE API CALL
-  var queryURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + searchCity + "&units=metric&cnt=5&appid=11a6edf7f55109a8876a082e0f89437e"
 
-  //USE AJAX TO DO API CALL TO DISPLAY CITY  AND WEATHER INFO
+  // FIRST AJAX API CALL GIVES THE LONGITUDE AND LATITUDE CO ORDINATES
   $.ajax({
-    url: queryURL,
+    url: LonLatURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
-    var date = new Date(response.list[1].dt * 1000)
-    date = date.toLocaleDateString()
-    console.log(date);
-      $(currentDayEl).text(searchCity + ", " + response.city.country + today.format(" (MM/DD/YYYY)"));
 
+    //SETS THE DATE AND CITY NAME
+    $(currentDayEl).text(searchCity + ", " + response[0].country + today.format(" (MM/DD/YYYY)"));
+    lat = response[0].lon;
+    lon = response[0].lat;
+    console.log("Longitude: " + lat);
+    console.log("Latitude: " + lon);
+  });
+
+  // SECOND AJAX CALL USES LONGITUDE AND LATITUDE VARS DEFINED IN PREV AJAX CALL
+  forecastURL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=5&appid=11a6edf7f55109a8876a082e0f89437e"
+  $.ajax({
+    url: forecastURL,
+    method: "GET",
+  }).then(function (responseForecast) {
+    console.log(responseForecast);
         // forecastDisplayer.append(newDiv)
         // newDiv.addClass("card col-2")
         // newDiv.append(newh5)
