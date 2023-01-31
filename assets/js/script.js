d6fb3fb985e8a090242780f87ed33a3ccbc29b40
umair shah <umairshah14@hotmail.com>
2023-01-31 13:11:35 +0000

    more updates, created function to generate recent search cities on search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+//GLOBAL VARIABLES
+var searchCity = "";
+var forecastURL = "";
+var searchHistory = []
+var today = moment();
+
+//DOM REFERENCES
 var searchBtnEl = $("#search-button");
 var currentDayEl = $("#currentDay");
 var additionalCitiesDiv = $(".additionalCities");
@@ -9,50 +16,51 @@ var jumboWeatherEl = $("#jumboWeatherIcon");
 var jumboDescriptionEl = $("#jumboDescription");
 var jumboHolderTitle = $("#todaysWeather");
 var jumboHolderEl = $("#jumboHolder");
-var searchCity = "";
-var forecastURL = "";
-var searchHistory = []
-var today = moment();
+var cityInfoEl = $("#cityInfo");
 
 $(currentDayEl).text("Please search for a city to begin");
+jumboHolderEl.hide()
+jumboWeatherEl.hide()
 
-function renderSearchHistory(city) {
-  var newCity =  $("<button>")
+
+//MAKE A NEW BUTTON FOR THE CITY THAT WAS JUST SEARCHED
+function addCity(city){
+  var newCity = $(("<button>"))
   newCity.text(city)
-  newCity.addClass("btn btnWidth2 btn-light")
-  newCity.attr("id", "search-button")
-  $("#cityInfo").append(newCity)
-
+  newCity.attr("id", "#search-button")
+  additionalCitiesDiv.append(newCity)
 }
+
 
 searchBtnEl.on("click", function (event) {
   event.preventDefault();
-
   // GRABS THE CITY USER TYPES IN
   searchCity = document.querySelector("#search-input").value.toUpperCase();
   console.log("city is: " + searchCity);
-  renderSearchHistory(searchCity)
+
+  //ADD THE BUTTON FOR THE CITY JUST SEARCHED
+  addCity(searchCity)
+
 
   // PASSES THE CITY INTO THE URL TO GRAB THE LONGITUDE AND LATITUDE
-  var LonLatURL =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    searchCity +
-    "&limit=1&appid=11a6edf7f55109a8876a082e0f89437e";
+  var LonLatURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchCity + "&limit=1&appid=11a6edf7f55109a8876a082e0f89437e";
 
   // FIRST AJAX API CALL GIVES THE LONGITUDE AND LATITUDE CO ORDINATES
   $.ajax({
     url: LonLatURL,
     method: "GET",
   }).then(function (response) {
-    // console.log(response);
+
+    // CHECKS IF A VALID CITY HAS BEEN ENTERED
     if (response.length === 0) {
       $(currentDayEl).text("No match found for: " + searchCity);
-      $("#infoHere").empty()
-      $("#5dayCast").text("")
-      $("#jumboHolder").text("")
+      jumboHolderEl.hide()
+      jumboWeatherEl.hide()
+      $("#infoHere").empty();
+      $("#5dayCast").text("");
       return;
     }
-  
+
     //SETS THE DATE AND CITY NAME
     $(currentDayEl).text(
       searchCity + ", " + response[0].country + today.format(" (DD/MM/YYYY)")
@@ -62,7 +70,6 @@ searchBtnEl.on("click", function (event) {
     var lat = response[0].lat.toString();
     var lon = response[0].lon.toString();
 
-    // console.log(lon, lat);
     // PASSES THE LAT AND LON COORDS INTO THE URL TO GRAB THE FORECAST INFO
     forecastURL = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&units=metric&cnt=5&appid=11a6edf7f55109a8876a082e0f89437e`;
 
@@ -71,14 +78,11 @@ searchBtnEl.on("click", function (event) {
       url: forecastURL,
       method: "GET",
     }).then(function (responseForecast) {
-
-     console.log(responseForecast);
-      // console.log(
-      //   responseForecast.city.coord.lon,
-      //   responseForecast.city.coord.lat
-      // );
+      console.log(responseForecast);
 
       // UPDATES JUMBOTRON INFORMATION
+      jumboHolderEl.show()
+      jumboWeatherEl.show()
       var jumboTempUnit = responseForecast.list[0].temp.day;
       var jumboTempIcon ="http://openweathermap.org/img/wn/" + responseForecast.list[0].weather[0].icon +"@2x.png";
       jumboHolderTitle.text("Today's weather:");
@@ -89,15 +93,16 @@ searchBtnEl.on("click", function (event) {
       jumboWeatherEl.attr("src", jumboTempIcon);
       jumboDescriptionEl.text("Expected weather: " + responseForecast.list[0].weather[0].description);
 
-      $("#infoHere").empty()
-      $("#5dayCast").text("5 DAY FORECAST: ")
+      $("#infoHere").empty();
+      $("#5dayCast").text("5 DAY FORECAST: ");
+
       for (let i = 0; i < responseForecast.list.length; i++) {
         var date = new Date(responseForecast.list[i].dt * 1000);
         date = date.toLocaleDateString();
-        var tempIcon = "http://openweathermap.org/img/wn/" + responseForecast.list[i].weather[0].icon + "@2x.png";
-        var temperature = responseForecast.list[i].temp.day
+        var tempIcon ="http://openweathermap.org/img/wn/" +responseForecast.list[i].weather[0].icon +"@2x.png";
+        var temperature = responseForecast.list[i].temp.day;
         temperature = temperature.toFixed(0) + "°C";
-        var windSpeed =  responseForecast.list[i].speed + " k/ph"
+        var windSpeed = responseForecast.list[i].speed + " k/ph";
         var humidity = responseForecast.list[i].humidity + "%";
         /// create the card
         let cardDiv = $("<div>");
@@ -109,26 +114,26 @@ searchBtnEl.on("click", function (event) {
         let cardLi3 = $("<li>");
         let attachForecastEl = $("<div>");
 
-    // attach the card to the page
-        attachForecastEl.append(cardDiv)
-        cardDiv.addClass("card col-12 spaceAround")
-        cardDiv.append(cardDate)
-        cardDate.text(date)
-        cardDiv.append(cardImg)
-        cardImg.attr("src", tempIcon)
-        cardImg.addClass("card-img-top")
-        cardDiv.append(cardUl)
-        cardUl.addClass("list-group list-group-flush")
-        cardUl.append(cardLi1)
-        cardUl.append(cardLi2)
-        cardUl.append(cardLi3)
-        cardLi1.text("Temp: " + temperature)
-        cardLi2.text("Wind: " + windSpeed)
-        cardLi3.text("Humidity: " + humidity)
-        cardLi1.addClass("list-group-item")
-        cardLi2.addClass("list-group-item")
-        cardLi3.addClass("list-group-item")
-        $("#infoHere").append(attachForecastEl)
+        // attach the card to the page
+        attachForecastEl.append(cardDiv);
+        cardDiv.addClass("card col-12 spaceAround");
+        cardDiv.append(cardDate);
+        cardDate.text(date);
+        cardDiv.append(cardImg);
+        cardImg.attr("src", tempIcon);
+        cardImg.addClass("card-img-top");
+        cardDiv.append(cardUl);
+        cardUl.addClass("list-group list-group-flush");
+        cardUl.append(cardLi1);
+        cardUl.append(cardLi2);
+        cardUl.append(cardLi3);
+        cardLi1.text("Temp: " + temperature);
+        cardLi2.text("Wind: " + windSpeed);
+        cardLi3.text("Humidity: " + humidity);
+        cardLi1.addClass("list-group-item");
+        cardLi2.addClass("list-group-item");
+        cardLi3.addClass("list-group-item");
+        $("#infoHere").append(attachForecastEl);
       }
     });
   });
